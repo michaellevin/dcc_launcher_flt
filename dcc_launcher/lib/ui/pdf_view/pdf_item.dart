2c55ed472bf426463fa97b99715d74b03597ae1b
Michael Levin <michael.levi.fx@gmail.com>
2023-12-21 01:35:53 +0200

    feat: add pdf viewer

--- dcc_launcher/lib/ui/pdf_view/pdf_item.dart
+++ dcc_launcher/lib/ui/pdf_view/pdf_item.dart
@@ -1,23 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:dcc_launcher/ui/list_item.dart';
 
+// import 'package:pdfx/pdfx.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+
 class PdfItem extends ListItem {
-  late final String pdfPath;
+  final String pdfPath;
 
   // Private constructor for use with the factory.
-  PdfItem._(String title, Key? key)
+  PdfItem._(this.pdfPath, String title, Key? key)
       : super(title, fileType: SupportedFileExtensions.pdf, key: key) {
-    pdfPath = title;
+    print('ini: $pdfPath');
   }
 
   // Factory constructor to create a PdfItem.
   factory PdfItem(String path, {Key? key}) {
-    String title = path.replaceAll("\\", "/").split('/').last;
-    return PdfItem._(title, key);
+    path = path.replaceAll('\\', '/');
+    String title = path.split('/').last;
+    return PdfItem._(path, title, key);
   }
 
   @override
   void onItemTap(BuildContext context) {
     print('subclass: $pdfPath');
+    // final PdfController controller = PdfController(
+    //   // document: PdfDocument.openAsset(pdfPath),
+    //   document: PdfDocument.openFile("assets/docs/Feldbach_1.pdf"),
+    // );
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: Container(
+              width: double.maxFinite,
+              height: MediaQuery.of(context).size.height *
+                  0.8, // 80% of screen height
+              // child: PdfView(controller: controller)
+              child: Scaffold(
+                body: SfPdfViewer.asset(pdfPath),
+              )),
+        );
+      },
+    );
   }
 }
--- dcc_launcher/macos/Flutter/GeneratedPluginRegistrant.swift
+++ dcc_launcher/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,18 @@
 import FlutterMacOS
 import Foundation
 
+import device_info_plus
 import screen_retriever
 import shared_preferences_foundation
+import syncfusion_pdfviewer_macos
+import url_launcher_macos
 import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   ScreenRetrieverPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SyncfusionFlutterPdfViewerPlugin.register(with: registry.registrar(forPlugin: "SyncfusionFlutterPdfViewerPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
