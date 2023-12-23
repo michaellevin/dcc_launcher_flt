2a32aa71f3dc942b364cd6d1ac9e7df3cd106bc5
Michael Levin <michael.levi.fx@gmail.com>
2023-12-23 05:07:22 +0200

    ui launcher uses config to open dccs

--- dcc_launcher/lib/ui/ui_launcher.dart
+++ dcc_launcher/lib/ui/ui_launcher.dart
@@ -1,44 +1,114 @@
 import 'package:flutter/material.dart';
+import 'package:yaml/yaml.dart';
+import 'dart:io';
+
+Future<String?> launchApplication(String appPath, List appEnv) async {
+  try {
+    if (await File(appPath).exists()) {
+      await Process.start(appPath, []);
+      return null; // Indicates success
+    } else {
+      return 'Application path does not exist: $appPath';
+    }
+  } catch (e) {
+    return 'Failed to launch the application: $e';
+  }
+}
+
+String getExecutablePath(app) {
+  /// Returns the executable path for the given app depends on platform
+  String executable = "";
+  switch (Platform.operatingSystem) {
+    case 'linux':
+      executable = app['linux_executable'] ?? "";
+      break;
+    case 'macos':
+      executable = app['macos_executable'] ?? "";
+      break;
+    case 'windows':
+      executable = app['windows_executable'] ?? "";
+      break;
+  }
+  return executable;
+}
+
+List<List<T>> chunkList<T>(List<T> list, int chunkSize) {
+  var chunks = <List<T>>[];
+  for (var i = 0; i < list.length; i += chunkSize) {
+    var end = (i + chunkSize < list.length) ? i + chunkSize : list.length;
+    chunks.add(list.sublist(i, end));
+  }
+  return chunks;
+}
 
 class AppLauncherWidget extends StatelessWidget {
+  /// The config file loaded as a YamlMap
+  final YamlMap? config;
+
+  const AppLauncherWidget({super.key, required this.config});
+
   @override
   Widget build(BuildContext context) {
+    if (config == null) {
+      // Config is not loaded yet, show a loading indicator
+      return const CircularProgressIndicator();
+    }
+
+    // Using null-aware operators to safely access config properties
+    // final Map<dynamic, dynamic> global_environment = config!['global_environment'] as Map<dynamic, dynamic>? ?? {};
+    final List<dynamic> applications =
+        config!['applications'] as List<dynamic>? ?? [];
+    final List<dynamic> applicationRows =
+        chunkList(applications, 3); // Split applications into chunks of 3
+    final List<dynamic> utilities =
+        config!['utilities'] as List<dynamic>? ?? [];
+
     return Container(
-      padding: EdgeInsets.all(10),
+      padding: const EdgeInsets.all(10),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Text('Applications', style: TextStyle(fontWeight: FontWeight.bold)),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              _buildIconButton('assets/icons/maya.png', 'Maya 2024'),
-              _buildIconButton('assets/icons/houdini.png', 'Houdini 19.5'),
-              _buildIconButton('assets/icons/blender.png', 'Blender 4.0.2'),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              _buildIconButton(
-                  'assets/icons/after_effects.png', 'After Effects 2023'),
-              _buildIconButton('assets/icons/nuke.png', 'Nuke'),
-            ],
-          ),
-          Divider(),
-          Text('Utilities', style: TextStyle(fontWeight: FontWeight.bold)),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              _buildIconButton('assets/icons/djv.png', 'DJV 2.0.8'),
-            ],
-          ),
+          const Text('Applications',
+              style: TextStyle(fontWeight: FontWeight.bold)),
+          ...applicationRows.map((chunk) {
+            return Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: chunk.map<Widget>((app) {
+                print(getExecutablePath(app));
+                return _buildIconButton(
+                  context,
+                  iconPath: app['icon'],
+                  label: '${app['name']} ${app['version']}',
+                  appPath: getExecutablePath(app),
+                  appEnv: app['environment'],
+                );
+              }).toList(),
+            );
+          }),
+          const Divider(),
+          const Text('Utilities',
+              style: TextStyle(fontWeight: FontWeight.bold)),
+          ...utilities.map((app) {
+            return Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                _buildIconButton(context,
+                    iconPath: app['icon'],
+                    label: '${app['name']} ${app['version']}',
+                    appPath: getExecutablePath(app)),
+              ],
+            );
+          }),
         ],
       ),
     );
   }
 
-  Widget _buildIconButton(String iconPath, String label) {
+  Widget _buildIconButton(BuildContext context,
+      {required String iconPath,
+      required String label,
+      required String appPath,
+      List? appEnv}) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Column(
@@ -52,9 +122,28 @@ class AppLauncherWidget extends StatelessWidget {
               child: Image.asset(iconPath, fit: BoxFit.contain),
             ),
             // iconSize: 24,
-            onPressed: () {
-              // Handle button press
-              print('Button pressed: $label');
+            onPressed: () async {
+              String? errorMessage =
+                  await launchApplication(appPath, appEnv ?? []);
+              if (errorMessage != null) {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: Text('No application found'),
+                      content: Text(errorMessage),
+                      actions: <Widget>[
+                        TextButton(
+                          child: Text('OK'),
+                          onPressed: () {
+                            Navigator.of(context).pop(); // Close the dialog
+                          },
+                        ),
+                      ],
+                    );
+                  },
+                );
+              }
             },
           ),
           Text(label),
